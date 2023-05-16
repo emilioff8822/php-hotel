e6559176f5719980a72b20fd7a7dd42258f48aec
emilioff8822 <emilioff@hotmail.it>
2023-05-16 13:57:55 +0200

    commit-foreach-list

--- index.php
+++ index.php
@@ -1,12 +1,71 @@
+<?php
+
+$hotels = [
+      [
+          'name' => 'Hotel Belvedere',
+          'description' => 'Hotel Belvedere Descrizione',
+          'parking' => true,
+          'vote' => 4,
+          'distance_to_center' => 10.4
+      ],
+      [
+          'name' => 'Hotel Futuro',
+          'description' => 'Hotel Futuro Descrizione',
+          'parking' => true,
+          'vote' => 2,
+          'distance_to_center' => 2
+      ],
+      [
+          'name' => 'Hotel Rivamare',
+          'description' => 'Hotel Rivamare Descrizione',
+          'parking' => false,
+          'vote' => 1,
+          'distance_to_center' => 1
+      ],
+      [
+          'name' => 'Hotel Bellavista',
+          'description' => 'Hotel Bellavista Descrizione',
+          'parking' => false,
+          'vote' => 5,
+          'distance_to_center' => 5.5
+      ],
+      [
+          'name' => 'Hotel Milano',
+          'description' => 'Hotel Milano Descrizione',
+          'parking' => true,
+          'vote' => 2,
+          'distance_to_center' => 50
+      ],
+  ];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+  <title>hotel</title>
 </head>
 <body>
-  <h1>hello</h1>
+  <div class="container text-center">
+  <ul>
+    <?php foreach($hotels as $hotel): ?>
+    <li>
+      <h2> <?php echo $hotel['name']; ?> </h2>
+      <p> <?php echo $hotel['description']; ?> </p>
+      <p>Parking : <?php echo $hotel ['parking'] ? 'Yes' : 'No'; ?> </p>
+      <p>Vote : <?php echo $hotel ['vote']; ?></p>
+      <p>Distance to center: <?php echo $hotel['distance_to_center']; ?> km </p>
+    </li>  
+    <?php endforeach; ?>
+  </ul>
+
+
+  </div>
+
+
 </body>
 </html>
